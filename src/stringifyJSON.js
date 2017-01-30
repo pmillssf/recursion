5a229a9bd28ff12ec65737525e30cba1f01748fc
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2017-01-30 15:14:32 -0800

    Get stringifyJSON to pass first five tests.
    Return correctly for strings, null, numbers, and booleans

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,32 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'string'){
+  	return '\"' + obj + '\"';
+  } 
+
+  if (obj === null){
+  	return 'null';
+  }
+
+  if (typeof obj === 'number'){
+  	return obj.toString();
+  }
+
+  if (typeof obj === 'boolean'){
+  	if(obj){
+  		return 'true';
+  	} else {
+  		return 'false';
+  	}
+  }
+
+
+
 };
+
+//figure out the type, 
+//find a way to ierate through the type
+//pass the obj/ array iteration of the type
+//returning the string of that iteration
+//run again
