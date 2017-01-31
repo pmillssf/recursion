ca014a485f1970b41dfa7ac7c38c3e7357f70452
Paul Mills <paulmills@tests-MacBook-Pro-2.local>
2017-01-30 22:48:16 -0800

    Set initial case for getElementsByClass

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,20 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className, node, index, outputArray) {
   // your code here
+  if (arguments.length === 1){
+  	if (_.contains(document.body.classList, className)){
+  		var outputArray = [];
+  		outputArray.push(document.body)
+  		return getElementsByClassName(className, document.body, 0, outputArray);
+  	} else{
+  		return getElementsByClassName(className, document.body, 0, [])
+  	}
+  } else {
+  }
 };
+
+// initiate by checking if the document.body node classList contatins the class
+// then run document.body through, checking if the childnodes match, and then if
+// the childnode has childnoes
